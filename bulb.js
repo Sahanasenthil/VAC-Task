f441da40a94eed4c4e0a19da538e9766861bf3fd
Sahana <123545315+Sahanasenthil@users.noreply.github.com>
2023-07-31 16:46:08 +0530

    Add files via upload

---
+++ bulb.js
@@ -0,0 +1,7 @@
+function on(){
+    document.getElementById('img').src="https://eportfoliojoebarker.files.wordpress.com/2015/05/light_bulb_on.png"
+}
+function off(){
+    document.getElementById('img').src="https://eportfoliojoebarker.files.wordpress.com/2015/05/light_bulb_off.png"
+
+}
